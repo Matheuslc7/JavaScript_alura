6fd867b61a66980507ffc3410fe369971d6c215c
Matheus Lima Cândido <121507091+Matheuslc7@users.noreply.github.com>
2023-11-30 20:20:52 -0300

    commit2

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 alert ('Bem vindo ao jogo do número secreto');
 let numeroSecreto = 7
-console.log (numeroSecreto)
-let chute = prompt('Escolha um número de 1 a 10');
+console.log (numeroSecreto);
+let chute;
+let tentativas = 1;
 
-if (chute == numeroSecreto) {
-    console.log ('Você acertou o número secreto, parabéns !');
+//enquanto chute for diferente de numeroSecreto
+while (chute != numeroSecreto) {
+
+    chute = prompt('Escolha um número de 1 a 10');
+    
+    if (chute == numeroSecreto) {
+        break;
+    }
+    else {
+        alert('Você errou, tente novamente !');
+        if (numeroSecreto > chute) {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        else {
+            alert(`O número secreto é menor que ${chute}`);
+        }
+        tentativas++;
+    }
+    
 }
-else {
-    alert('Você errou, tente novamente !')
-}
+let pluralTentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
+    
+alert(`Você acertou o número secreto ${numeroSecreto}, com  ${tentativas}  ${pluralTentativas}, parabéns !`);
